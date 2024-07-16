65b5a13a68635ea29d697677bcfe6be556b5bde3
Subasish Chakraborty <subasis14@gmail.com>
2024-07-16 16:00:26 +0530

    added javascript

---
+++ assets/dashboard/js/fixed_menu.js
@@ -0,0 +1,46 @@
+
+"use strict";
+$(document).ready(function () {
+
+    if($(window).width()>767){
+        $(window).on("scroll", function () {
+            fixed_menu();
+            if ($(this).scrollTop() > 50) {
+                $('.fixed_menu #left').addClass('rightsidebar-without-nav');
+            } else {
+                $('.fixed_menu #left').removeClass('rightsidebar-without-nav');
+            }
+
+            if ($(this).scrollTop() > 50 && $(this).scrollTop() < 100) {
+                $('.fixed_menu #left').addClass('rightsidebar-without-nav-small');
+            } else {
+                $('.fixed_menu #left').removeClass('rightsidebar-without-nav-small');
+            }
+
+        });
+        fixed_menu();
+    }else{
+        $(".menu_scroll").slimScroll({
+            destroy: true,
+            height: '100%'
+        });
+        $("#top,#left").removeClass("fixed");
+        $("body").removeClass("fixedMenu_header");
+    }
+    function fixed_menu() {
+        $(".fixedMenu_header .wrapper").css("margin-top",$("#top").height());
+        if($("#left").hasClass("rightsidebar-without-nav")){
+            $(".menu_scroll").slimscroll({
+                height: $(window).height(),
+                size: '5px',
+                opacity: 0.2
+            });
+        }else{
+            $(".menu_scroll").slimscroll({
+                height: $(window).height()-$("#top").height(),
+                size: '5px',
+                opacity: 0.2
+            });
+        }
+    }
+});
